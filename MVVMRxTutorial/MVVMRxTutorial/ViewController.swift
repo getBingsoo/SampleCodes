463bac54dd3e16c34879a9f3d830a35c4c7e6d85
최리나 <getBingsoo@gmail.com>
2021-06-15 18:23:37 +0900

    MVVMRxTutorial 프로젝트 생성

---
+++ MVVMRxTutorial/MVVMRxTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMRxTutorial
+//
+//  Created by 60067667 on 2021/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
